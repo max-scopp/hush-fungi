c42403b33b27cb74faae569224d2d4e29810771c
Max Scopp <me@maxscopp.de>
2024-03-18 22:37:47 +0100

    mac fixes

--- src/main/main.ts
+++ src/main/main.ts
@@ -266,6 +266,7 @@ function whenReadyCreateMainWindow() {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
         if (mainWindow === null) createWindow();
+        focusMainWindow();
       });
     })
     .catch(console.log);
