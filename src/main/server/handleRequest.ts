f1312db50c34c1aa1b5a75cdd65e17078fb479b3
Max Scopp <me@maxscopp.de>
2024-04-07 13:57:34 +0200

    cleanup handle req

--- src/main/server/handleRequest.ts
+++ src/main/server/handleRequest.ts
@@ -1,7 +1,5 @@
 import { IncomingMessage, OutgoingMessage } from "http";
 import { APP_INTERNAL_SERVER_ADRESS } from "../../shared/constants";
-import { SETTINGS_WINDOW_START_URL } from "../constants";
-import { mainWindow } from "../windows/mainWindow";
 
 export function handleRequest(
   request: IncomingMessage,
@@ -9,23 +7,14 @@ export function handleRequest(
 ) {
   const url = new URL(request.url, APP_INTERNAL_SERVER_ADRESS);
 
+  // focuses the application from the browser
+  // response.end(`<head>
+  //     <meta http-equiv="Refresh" content="0; URL=hush-fungi://focus" />
+  //     <script>setTimeout(() => close(), 300)</script>
+  //   </head><body>You're done!</body>`);
+
   switch (true) {
-    case url.searchParams.has("auth_callback") &&
-      url.searchParams.has("code") &&
-      url.searchParams.has("state"): {
-      const startUrl = SETTINGS_WINDOW_START_URL;
-      const params = /(\?.*$)/.exec(request.url)[0];
-
-      const redirected = startUrl + params;
-
-      mainWindow.webContents.loadURL(redirected);
-      response.end(`<head>
-          <meta http-equiv="Refresh" content="0; URL=hush-fungi://focus" />
-          <script>setTimeout(() => close(), 300)</script>
-        </head><body>You're done!</body>`);
-      return;
-    }
     default:
-      response.end(`Nope.`);
+      response.end(``);
   }
 }
